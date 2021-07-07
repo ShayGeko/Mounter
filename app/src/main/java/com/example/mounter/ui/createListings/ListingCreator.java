859280a4c917777da41848dfebcbde7c2600e2e2
jperalgo <jperalgo@sfu.ca>
2021-07-06 21:20:38 -0700

    ✨Added submit and back button

--- app/src/main/java/com/example/mounter/ui/createListings/ListingCreator.java
+++ app/src/main/java/com/example/mounter/ui/createListings/ListingCreator.java
@@ -2,17 +2,44 @@ package com.example.mounter.ui.createListings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 
 import com.example.mounter.R;
 
 public class ListingCreator extends AppCompatActivity {
 
+    private Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing_creator);
+
+        ImageButton submit = findViewById(R.id.submit);
+        ImageButton back = findViewById(R.id.back);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Log.i("MyApp", "Clicked on submit");
+                intent = new Intent(getApplicationContext(), );
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Log.i("MyApp", "Clicked on back");
+                intent = new Intent(getApplicationContext(), ChooseListing.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
